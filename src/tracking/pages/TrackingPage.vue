a3159a1d117a98c2d5f87183ba9014c067c4c6d0
Fabricio Huillca <u202118067@upc.edu.pe>
2025-10-04 02:40:57 -0500

    feat: updated component

--- src/tracking/pages/TrackingPage.vue
+++ src/tracking/pages/TrackingPage.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="tracking-page">
+<<<<<<< Updated upstream
     <h1>Tracking</h1>
+=======
+<<<<<<< Updated upstream
+    <h1>Tracking</h1>
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
     <SiteList />
   </div>
 </template>

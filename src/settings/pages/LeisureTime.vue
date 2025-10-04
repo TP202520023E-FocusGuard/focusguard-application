fbf5f09472e112aa15012fd937caf4adf2faa9d6
Fabricio Huillca <u202118067@upc.edu.pe>
2025-10-04 02:37:27 -0500

    feat: added time configuration

---
+++ src/settings/pages/LeisureTime.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <LeisureTimeConfig />
+  </v-container>
+</template>
+
+<script>
+import LeisureTimeConfig from '../components/LeisureTimeConfig.vue';
+
+export default {
+  name: 'LeisureTimePage',
+  components: {
+    LeisureTimeConfig
+  }
+};
+</script>
